b6598fd928acd878241f74da906e59ea4b94d3a2
Kim Kunwoo <drama0708@gmail.com>
2017-02-07 14:13:28 +0900

    build.sh changed

--- shell_script/build.sh
+++ shell_script/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 cd ..
-git pull https://github.com/ultimatelife/kunwoo.kim.aka
+#git pull https://github.com/ultimatelife/kunwoo.kim.aka
+git fetch --all
+git reset --hard origin/master
 npm install
